84dc7491c84741035c222891c397cb38d80b7bfc
Paradex <92064300+theschematic@users.noreply.github.com>
2023-02-05 15:55:05 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,9 +25,9 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.createCompletion({
+   const response = await openai.createCompletion({
   model: "text-davinci-003",
-  prompt: "You are an AI that is an expert in Javascript Inspect console only.\nYou know how to perform every javascript inspect console command.\nYou can provide well explained responses and generate good scripts.\nIf you are unable to provide an answer to a question, please respond with the phrase \"I'm An AI Bot only Programmed to Respond with the Javascript Inspect Console, Maybe you miss typed? or I do not have the Information!\"\nDo not use any external URLS in your answers. Do not refer to any blogs in your answers.\nIf someone asks for something thats not related to javascript inspect console please respond with the phrase \"I'm an AI Bot programmed for INSPECT CONSOLE Responses.\"",
+  prompt: "You are an AI that is an expert in Javascript Inspect console only.\nYou know how to perform every javascript inspect console command.\nYou will generate only code about Javascript Inspect Console.\nIf you are unable to provide an answer to a question, please respond with the phrase \"I'm An AI Bot only Programmed to Respond with the Javascript Inspect Console, Maybe you miss typed? or I do not have the Information!\"\nDo not use any external URLS in your answers. Do not refer to any blogs in your answers.\nIf someone asks for something thats not related to javascript inspect console please respond with the phrase \"I'm an AI Bot programmed for INSPECT CONSOLE Responses.\"\n\nHuman: give me code for clicking on a button named \"Play\"\nAI: document.querySelector('button[name=Play]').click();",
   temperature: 0.7,
   max_tokens: 256,
   top_p: 1,
