04f6cddd0263a0a6c63610cb46f03c14143691a2
Paradex <92064300+theschematic@users.noreply.github.com>
2023-02-05 19:50:00 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,19 +25,52 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
+    let model = "text-davinci-003";
+    let temperature = 0;
+    let max_tokens = 225;
+    let top_p = 1;
+    let frequency_penalty = 0.5;
+    let presence_penalty = 0;
+
+    if (prompt.startsWith("[easy]")) {
+      temperature = 3;
+    } else if (prompt.startsWith("[k-easy]")) {
+      // adjust parameters for very simple essays for kindergarteners
+    } else if (prompt.startsWith("[complex]")) {
+      // adjust parameters for complex essays for adults
+    } else if (prompt.startsWith("[silly]")) {
+      // adjust parameters for silly essays
+    } else if (prompt.startsWith("[misspell]")) {
+      // adjust parameters for essays with misspelled words
+    } else if (prompt.startsWith("[s-misspell]")) {
+      // adjust parameters for essays with slightly misspelled words
+    } else if (prompt.startsWith("[easy-misspell]")) {
+      // adjust parameters for easy-to-read essays with slightly misspelled words
+    } else if (prompt.startsWith("[requireURLS]")) {
+      // adjust parameters for essays with URLs
+    } else if (prompt.startsWith("[plagiarizedOff]")) {
+      // adjust parameters for non-plagiarized essays
+    }
+
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: model,
       prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 225, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      temperature: temperature,
+      max_tokens: max_tokens,
+      top_p: top_p,
+      frequency_penalty: frequency_penalty,
+      presence_penalty: presence_penalty
     });
 
-    res.status(200).send({
-      bot: response.data.choices[0].text
-    });
+    if (!response.data.choices[0].text) {
+      res.status(200).send({
+        bot: "I'm An AI Bot Programmed to Write Essays!"
+      });
+    } else {
+      res.status(200).send({
+        bot: response.data.choices[0].text
+      });
+    }
 
   } catch (error) {
     console.error(error)
